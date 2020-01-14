50d125522cf3b5c396e759a2bbe41963f3e2aaf2
suicodave <suico.dave@gmail.com>
2020-01-14 16:50:46 +0800

    initial commit

---
+++ Models/TodoItem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApiTraining.Models
+{
+    public class TodoItem
+    {
+        public long Id { get; set; }
+
+        public string Name { get; set; }
+
+        public bool IsComplete { get; set; }
+
+
+
+    }
+}
