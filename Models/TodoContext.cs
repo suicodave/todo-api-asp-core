e237f03e2dab7e1ea94e9042233455b336fc97d7
suicodave <suico.dave@gmail.com>
2020-01-14 18:02:20 +0800

    1st TodoItem migration

---
+++ Models/TodoContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace WebApiTraining.Models
+{
+    public class TodoContext : DbContext
+    {
+        public TodoContext(DbContextOptions<TodoContext> options) : base(options)
+        {
+        }
+
+        public DbSet<TodoItem> TodoItems { get; set; }
+
+
+
+    }
+
+}
